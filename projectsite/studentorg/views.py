b75b33c2bc6a36e54fde1ae48d0a7cbf95247032
ejlanuza0123 <ejlanuza0123@gmail.com>
2025-04-02 19:55:14 +0800

    Added fix

--- projectsite/studentorg/views.py
+++ projectsite/studentorg/views.py
@@ -1,4 +1,5 @@
-from django.views.generic import ListView, CreateView
+from django.views.generic import ListView
+from django.views.generic.edit import CreateView, UpdateView
 from django.urls import reverse_lazy
 from studentorg.models import Organization
 from studentorg.forms import OrganizationForm 
@@ -18,3 +19,9 @@ class OrganizationCreateView(CreateView):
     form_class = OrganizationForm
     template_name = 'org_add.html'
     success_url = reverse_lazy('organization-list')
+
+class OrganizationUpdateView(UpdateView):
+    model = Organization
+    form_class = OrganizationForm  # Corrected from 'from class'
+    template_name = 'org_edit.html'  # Corrected from 'template name'
+    success_url = reverse_lazy('organization-list') 
